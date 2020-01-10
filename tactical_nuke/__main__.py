54e0afd0c07bd16b917c5c8f909c542e74f6769b
Travis McKee <jarvmc@amazon.com>
2020-01-10 18:42:04 -0500

    Remove pycache, slight refactor

--- tactical_nuke/__main__.py
+++ tactical_nuke/__main__.py
@@ -2,50 +2,9 @@
 
 import boto3
 import click
-import datetime
 import sys
 
 import tactical_nuke.utils.s3 as s3_utils
-import tactical_nuke.utils.date as date_utils
-
-
-def get_s3_bucket_predicate(**kwargs):
-    predicates = []
-
-    if kwargs.get('earlier_than'):
-        earlier_date = date_utils.str2datetime(kwargs.get('earlier_than'))
-        predicates.append(lambda bucket: bucket.get('CreationDate') < earlier_date)
-
-    if kwargs.get('later_than'):
-        later_date = date_utils.str2datetime(kwargs.get('later_than'))
-        predicates.append(lambda bucket: bucket.get('CreationDate') > later_date)
-
-    if kwargs.get('created_on'):
-
-        def date_on(bucket):
-            created_date = date_utils.str2datetime(kwargs.get('created_on'))
-            next_date = created_date + datetime.timedelta(days=1)
-
-            bucket_created = bucket.get('CreationDate')
-
-            return created_date < bucket_created < next_date
-
-        predicates.append(date_on)
-
-    if kwargs.get('starts_with'):
-        predicates.append(lambda bucket: bucket.get('Name').startswith(kwargs.get('starts_with')))
-
-    if kwargs.get('ends_with'):
-        predicates.append(lambda bucket: bucket.get('Name').endswith(kwargs.get('ends_with')))
-
-    if kwargs.get('contains'):
-        predicates.append(lambda bucket: kwargs.get('contains') in bucket.get('Name'))
-
-    if kwargs.get('exclude'):
-        predicates.append(lambda bucket: bucket.get('Name') not in kwargs.get('exclude'))
-
-    return lambda x: all([p(x) for p in predicates])
-
 
 @click.group()
 def cli():
@@ -68,10 +27,7 @@ def s3(**kwargs):
 
     dry = not kwargs.get('delete')
 
-    s3_utils.delete_s3_buckets(
-        s3_client,
-        get_s3_bucket_predicate(**kwargs),
-        dry=dry)
+    s3_utils.delete_s3_buckets(s3_client, dry=dry, **kwargs)
 
 
 if __name__ == '__main__':
